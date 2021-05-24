3b74732bb0122de47ca0df4cfbdc5d1b975c3544
jkim276 <jkim276@k1-admui-MacBookPro.local>
2021-05-24 17:38:34 +0900

    subDomain

--- mergeTwoLists.py
+++ mergeTwoLists.py
@@ -5,6 +5,7 @@ class Solution:
         if l1 and l2:
             if l1.val > l2.val:
                 l1, l2 = l2, l1
+            # use an iteration 
             l1.next = self.mergeTwoLists(l1.next, l2)
         return l1 or l2
 
