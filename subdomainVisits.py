f32ffa6d697d06bca7b3dc01b76e8c742881b29c
jkim276 <jkim276@k1-admui-MacBookPro.local>
2021-05-24 17:39:52 +0900

    subDomain

---
+++ subdomainVisits.py
@@ -0,0 +1,16 @@
+## https://leetcode.com/problems/subdomain-visit-count/
+
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+    	D = {}
+    	for c in cpdomains:
+    		s = c.replace('.',' ').split()
+    		n = int(s[0]) ## count (int)
+            # for loop backward
+    		for i in range(len(s)-1,0,-1):
+    			t = ".".join(s[i:])
+    			D[t] = D[t] + n if t in D else n
+    	return [str(D[i])+" "+i for i in D]
+
+
+
