44ab22f51bc11f002e9c2e910f2c5abbc7fd2346
jiyoung <skql951123@gmail.com>
2021-05-31 20:47:39 +0900

    added

--- plusOne.py
+++ plusOne.py
@@ -3,7 +3,7 @@
 class Solution(object):
     def plusOne(self, digits):
         if digits[-1] == 9:
-            if len(digits) == 1:  # Already a 9
+            if len(digits) == 1:  
                 return [1, 0]
             return self.plusOne(digits[:-1]) + [0]
         else:
