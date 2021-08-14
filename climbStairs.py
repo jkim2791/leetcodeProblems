9a906ef4bfc8a67e3a83514d5f6822865c0dd49b
skql951123@gmail.com <jkim2791@berkeley.edu>
2021-08-14 19:07:18 +0900

    climb st

---
+++ climbStairs.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/climbing-stairs/submissions/
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one, two = 1, 1
+        for i in range(n-1):
+            tmp = one
+            one = one + two
+            two = tmp
+        return one
+        
