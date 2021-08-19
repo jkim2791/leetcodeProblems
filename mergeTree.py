5bab6fa95c6ea77e838e556348427e1860233918
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 <jkim2791@berkeley.edu>
2021-08-19 14:44:22 +0900

    add pr

---
+++ mergeTree.py
@@ -0,0 +1,21 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def mergeTree(self, t1:TreeNode, t2:TreeNode) -> TreeNode:
+        # return none when both trees have empty nodes
+        if not t1 and not t2:
+            return None
+        # implement values for each tree
+        v1 = t1.val if t1 else 0
+        v2 = t2.val if t2 else 0
+        # add values from t1, t2 when the value is not 0
+        root = TreeNode(v1+v2)
+        # recursive call for t1 and t2
+        root.left = self.mergeTree(t1.left if t1 else None, t2.left if t2 else None)
+        root.right = self.mergeTree(t1.right if t1 else None, t2.right if t2 else None)
+
+        return root        
